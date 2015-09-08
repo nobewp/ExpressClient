9d1e68086394b4d78ee27e048fc6145bd41fce1b
wangpeng2 <wangpeng2@xywy.com>
2015-09-08 08:55:10 +0800

    t

---
+++ ExpressClient/src/com/nobe/expressclient/MainActivity.java
@@ -0,0 +1,25 @@
+package com.nobe.expressclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * 
+ * @author 王鹏
+ * @version
+ * @2015-9-8上午8:43:21
+ *
+ */
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
